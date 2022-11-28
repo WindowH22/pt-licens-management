d110e56dde422ce002d97310bbf11296581ae536
WindowH22 <dlckdgml1235@naver.com>
2022-11-28 20:28:05 +0900

    feat: Add Method

--- src/main/java/com/fastcampus/pt/repository/pass/BulkPassRepository.java
+++ src/main/java/com/fastcampus/pt/repository/pass/BulkPassRepository.java
@@ -2,5 +2,10 @@ package com.fastcampus.pt.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BulkPassRepository extends JpaRepository<BulkPassEntity,Integer> {
+    // WHERE status = : status AND startedAT
+    List<BulkPassEntity>  findByStatusAndStartedAtGreaterThan(BulkPassStatus status, LocalDateTime startedAt);
 }
