f7b295a00522bbdda8424976eda73fc3541b8e2d
WindowH22 <75984536+WindowH22@users.noreply.github.com>
2022-11-30 20:18:59 +0900

    Update SendNotificationBeforeClassJobConfig.java

--- src/main/generated/job/notification/SendNotificationBeforeClassJobConfig.java
+++ src/main/generated/job/notification/SendNotificationBeforeClassJobConfig.java
@@ -1,4 +1,4 @@
-package job.notification;
+package com.fastcampus.pt.job.notification;
 
 import com.fastcampus.pt.repository.booking.BookingEntity;
 import com.fastcampus.pt.repository.notification.NotificationEntity;
