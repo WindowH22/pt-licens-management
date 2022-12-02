92fb18c77a3f50c90149c2dfb37cc0de30c936f8
WindowH22 <dlckdgml1235@naver.com>
2022-12-02 21:58:24 +0900

    feat: Add JobLauncher

---
+++ src/main/java/com/fastcampus/pt/controller/JobLauncherRequest.java
@@ -0,0 +1,22 @@
+package com.fastcampus.pt.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+
+import java.util.Properties;
+
+@Getter
+@Setter
+@ToString
+public class JobLauncherRequest {
+    private String name;
+    private Properties jobParameters;
+
+    public JobParameters getJobParameter(){
+        return new JobParametersBuilder(this.jobParameters).toJobParameters();
+    }
+
+}
